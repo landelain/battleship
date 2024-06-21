cf3cc670cd55c9a1d6668b5d22c67bcfc9d5a229
Lucas Massot <lucas.massot@allemagne.polytechnique.fr>
2024-06-21 14:25:41 +0200

    early version of server and client

--- client.c
+++ client.c
@@ -11,5 +11,63 @@
 
 
 int main(int argc, char* argv[]){
+
+    if(argc < 3){
+        fprintf(stderr, "Error : missing arguments");
+        return 1;
+    }
+
+    char* ip_address = argv[1];
+    int port = atoi(argv[2]);
+
+
+    // socket creation
+
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd == -1) {
+        fprintf(stderr, "Error : socket creation");
+        return 1;
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+
+    server_addr.sin_port = htons(port);  
+    if (inet_pton(AF_INET, ip_address, &server_addr.sin_addr) != 1) {
+        fprintf(stderr, "Error : ip address");
+        close(sockfd);
+        return 1;
+    }
+
+    // initial Hello message 
+
+    char msg[100] = "hello";
+
+    if (sendto(sockfd, msg, strlen(msg), 0, (const struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
+        fprintf(stderr, "Error : sending message");
+        close(sockfd);
+        return 1;
+    }  
+
+    socklen_t server_addr_len = sizeof(server_addr);
+    if (recvfrom(sockfd, msg, sizeof(msg) - 1, 0, (struct sockaddr *)&server_addr, &server_addr_len) == -1){
+        fprintf(stderr, "Error : receiving message");
+        close(sockfd);
+        return 1;
+    }
+
+    if(strcmp(msg, "OK") != 0){
+        fprintf(stderr, "Error : connection failed");
+        close(sockfd);
+        return 1;
+    }
+
+    while(1){
+
+        
+    }
+
     return 0;
 }
--- server.c
+++ server.c
@@ -9,7 +9,99 @@
 #include <errno.h>
 #include <arpa/inet.h>
 
+//network
+int port;
+int connected = 0; 
+struct sockaddr_in client1 = {0};
+struct sockaddr_in client2 = {0};
+
+//logic
+int size, carriers, battleships, destroyers, submarines; 
+
+
+int is_new_client(struct sockaddr_in address);
+int handle_new_client(struct sockaddr_in address);
 
 int main(int argc, char* argv[]){
+
+    if(argc<7){
+        fprintf(stderr, "Error : missing arguments");
+        return 1;
+    }
+
+    port = atoi(argv[1]);
+
+    size = atoi(argv[2]);
+    carriers = atoi(argv[3]);
+    battleships = atoi(argv[4]);
+    destroyers = atoi(argv[5]);
+    submarines = atoi(argv[6]);
+
+    // socket creation 
+
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        fprintf(stderr,"Error: socket creation failed\n");
+        return 1;
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        fprintf(stderr,"Error: bind failed\n");
+        close(sockfd);
+        return 1;
+    }
+
+    printf("\nThe server is listening\n\n");
+
+    while(1){
+
+        struct sockaddr_in temp_client_addr;
+        socklen_t temp_client_addr_len = sizeof(temp_client_addr);
+        char msg[100];
+
+        ssize_t received_len = recvfrom(sockfd, msg, sizeof(msg), 0, (struct sockaddr *)&temp_client_addr, &temp_client_addr_len);
+        if (received_len > 0) {
+            msg[received_len] = '\0';
+        }
+
+        if(is_new_client(temp_client_addr) == 0){
+            if(handle_new_client(temp_client_addr) == -1){
+                fprintf(stderr, "Error : Already 2 clients connected");
+                return 1;
+            }
+        }
+        
+
+
+    }
     return 0;
+}
+
+int is_new_client(struct sockaddr_in address){
+
+    if(client1.sin_addr.s_addr == address.sin_addr.s_addr && client1.sin_port == address.sin_port){
+        return 1;
+    }
+
+    if(client2.sin_addr.s_addr == address.sin_addr.s_addr && client2.sin_port == address.sin_port){
+        return 2;
+    }
+
+    return 0;
+}
+
+int handle_new_client(struct sockaddr_in address){
+
+    if(connected < 2){
+        connected += 1;
+        return 0;
+    }
+    
+    return -1;
 }
